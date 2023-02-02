0e1d725bc8b6149a17d517c73190ded00a21f073
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-02-02 17:43:49 +0200

    Add dropdowns with ViewBag and ViewData in Product Controller and View

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -17,22 +18,28 @@ namespace BulkyBook.Models {
         [Required]
         public string Author { get; set; }
         [Required]
+        [DisplayName("List Price (MSRP)")]
         [Range(1, 10000)]
         public double ListPrice { get; set; }
         [Required]
         [Range(1, 10000)]
         public double Price { get; set; }
         [Required]
+        [DisplayName("Price (50)")]
         [Range(1, 10000)]
         public double Price50 { get; set; }
         [Required]
+        [DisplayName("Price (100)")]
         [Range(1, 10000)]
         public double Price100 { get; set; }
+        [DisplayName("Image URL")]
         public string ImageUrl { get; set; }
+        [DisplayName("Category")]
         public int CategoryId { get; set; }
         [Required]
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+        [DisplayName("Cover Type")]
         public int CoverTypeId { get; set; }
         [Required]
         [ForeignKey("CoverTypeId")]
--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,7 +2,9 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace BulkyBookWeb.Controllers {
     [Area("Admin")]
@@ -19,8 +21,20 @@ namespace BulkyBookWeb.Controllers {
 
         // GET
         public IActionResult Upsert(int? id) {
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(u => new SelectListItem {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
             if (id == null || id == 0) {
                 // create product
+                ViewBag.CategoryList = CategoryList;
+                ViewData["CoverTypeList"] = CoverTypeList;
+                return View(product);
             } else {
                 // update product
             }
