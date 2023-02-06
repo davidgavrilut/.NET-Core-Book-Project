87f154715612276b567b0d5cb39427d9f4d341c5
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-02-06 21:17:13 +0200

    Implement sweetalert2 for image upload validation

---
+++ src/BestBookWeb/wwwroot/js/sweetalerts.js
@@ -0,0 +1,11 @@
+﻿function validateInput() {
+    if (!document.getElementById("uploadBox").value) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please an image!'
+        });
+        return false;
+    }
+    return true;
+}
