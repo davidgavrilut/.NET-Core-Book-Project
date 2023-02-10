1b547bc34ef4838c6146ac2cd935b3b67fb056f9
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-02-10 18:15:27 +0200

    Fix ImageUrl

--- src/BestBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ src/BestBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -46,23 +46,38 @@ namespace BestBook.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(ProductViewModel obj, IFormFile? file) {
-            if (ModelState.IsValid) {   
+            if (ModelState.IsValid) {
                 string wwwRootPath = _hostEnvironment.WebRootPath;
                 if (file != null) {
                     string fileName = Guid.NewGuid().ToString();
                     var uploads = Path.Combine(wwwRootPath, @"img\products");
                     var extension = Path.GetExtension(file.FileName);
-                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create)) {
-                        file.CopyTo(fileStreams);
+
+                    if (obj.Product.ImageUrl != null) {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath)) {
+                            System.IO.File.Delete(oldImagePath);
+                        }
                     }
-                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                        using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create)) {
+                            file.CopyTo(fileStreams);
+                        }
+                        obj.Product.ImageUrl = @"\img\products\" + fileName + extension;
+                    }
+
+                    if (obj.Product.Id == 0) {
+                        _unitOfWork.Product.Add(obj.Product);
+                        TempData["success"] = "Product created successfully";
+                    } else {
+                        _unitOfWork.Product.Update(obj.Product);
+                        TempData["success"] = "Product updated successfully";
+                    }
+
+                    _unitOfWork.Save();
+
+                    return RedirectToAction("Index");
                 }
-                _unitOfWork.Product.Add(obj.Product);
-                _unitOfWork.Save();
-                TempData["success"] = "Product created successfully";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
+                return View(obj);
         }
 
         public IActionResult Delete(int? id) {
