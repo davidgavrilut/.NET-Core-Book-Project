a458e09621216b493ff376d11c2f27af74f7d56a
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-02-24 16:49:46 +0200

    Create ApplicationUser model that extends IdentityUser class, add to ApplicationDbContext, migration and db update

--- src/BestBook.DataAccess/Data/ApplicationDbContext.cs
+++ src/BestBook.DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace BestBook.Data {
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
