3026e2ff174741efc2cbcd94e05d15052cfbcdd5
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-02-25 16:08:48 +0200

    Add Company Model to context and db (migration)

--- src/BestBook.DataAccess/Data/ApplicationDbContext.cs
+++ src/BestBook.DataAccess/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace BestBook.Data {
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
     }
 }
