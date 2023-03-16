1b5ec74c4125bc388fc0a392126aceaab0838f1b
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-03-16 12:09:09 +0200

    OrderController GetAll restrict Orders view access depending on role

--- src/BestBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ src/BestBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,9 +1,13 @@
 ﻿using BestBook.DataAccess.Repository.IRepository;
 using BestBook.Models;
+using BestBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BestBookWeb.Areas.Admin.Controllers;
 [Area("Admin")]
+[Authorize]
 public class OrderController : Controller {
     private readonly IUnitOfWork _unitOfWork;
     public OrderController(IUnitOfWork unitOfWork) {
@@ -14,7 +18,14 @@ public class OrderController : Controller {
     }
     #region API CALLS
     public IActionResult GetAll() {
-        IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+        IEnumerable<OrderHeader> orderHeaders;
+        if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee)) {
+        orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+        } else {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "ApplicationUser");
+        }
         return Json(new {
             data = orderHeaders
         });
